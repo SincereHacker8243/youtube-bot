bb9ff14ae1874a4c02aa7e89eb66813fa9056a3b
SincereHacker8243 <67733503+SincereHacker8243@users.noreply.github.com>
2021-03-21 23:52:03 +0530

    Update example-subscribe.py

--- example-subscribe.py
+++ example-subscribe.py
@@ -1,7 +1,7 @@
 from Bot import Bot
 
 # your location to chromedriver executable
-bot = Bot('/Users/**/Downloads/chromedriver')
+bot = Bot(C/GTAV/chromedriver')
 
 bot.goto_youtube() \
     .login(email='your@email', password='password') \
